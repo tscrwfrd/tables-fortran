dd38f2cdc6553eb7dbcec5111137462add52a5be
Trevor Crawford <crawford.trevor@gmail.com>
2025-03-09 16:24:56 -0600

    Added column data type support.

--- app/main.f90
+++ app/main.f90
@@ -12,10 +12,24 @@ program main
   call add_f32_column(tbl, [19.1, 210.0, 311.0, 22.0])
   call remove_f32_column_idx(tbl, 2)
 
+  print *, "EXAMPLE FILE"
   do i = 1, tbl%ncols
-    print *, "Column", i, ":"
-    print *, "  Values:", tbl%columns(i)%values
+      print *, "Column", i, ":"
+      print *, "  Values:", tbl%columns(i)%values
   end do
+
+  print *, "=============================="
+
+  do i = 1, tbl%ncols
+      select type (d => tbl%columns(i)%data)
+          type is (F32)
+              print *, "Column", i, ":"
+              print *, "  Values:", d%values
+          class default
+              print *, "Warning: Unknown type"
+      end select
+  end do
+
   call destroy_table(tbl)
 
 
--- src/tables_fortran.f90
+++ src/tables_fortran.f90
@@ -1,29 +1,50 @@
 module tables_fortran
     use iso_c_binding
-    ! use iso_fortran_env, only: uint64
+
     implicit none
     private
+
+
+    enum, bind(c)
+      enumerator :: COL_TYPE_UNKNOWN = 0
+      enumerator :: COL_TYPE_I32 = 1
+      enumerator :: COL_TYPE_F32 = 2
+    end enum
+
+    type ColumnData
+    end type
+
+    type, extends(ColumnData) :: I32
+        integer(kind=c_int32_t), allocatable :: values(:)
+    end type I32
+
+    type, extends(ColumnData) :: F32
+        real(kind=c_float), allocatable :: values(:)
+    end type F32
     
     ! validity bitmap, LSB
     type :: BitMap
         integer(kind=c_int64_t), allocatable :: bits(:)
     end type BitMap
+
     
     type :: FortranColumn
         type(BitMap) :: validity
         integer(kind=c_int32_t), allocatable :: offsets(:)
         real(kind=c_float), allocatable :: values(:)
+        class(ColumnData), allocatable :: data
     end type FortranColumn
     
     type :: FortranTable
         integer :: ncols = 0
         integer :: nrows = 0
+        logical :: has_headers = .false.
         type(FortranColumn), allocatable :: columns(:)
     end type FortranTable
 
     public FortranTable
     public add_f32_column, remove_f32_column_idx, destroy_table
-    
+    public F32, I32
 contains
 
   subroutine allocate_table(table, nrows, ncols)
@@ -36,9 +57,21 @@ contains
 
     allocate(table%columns(ncols))
     do i = 1, ncols
+
       allocate(table%columns(i)%validity%bits(2))
       allocate(table%columns(i)%offsets(2))
       allocate(table%columns(i)%values(nrows))
+
+      allocate(F32 :: table%columns(i)%data)
+      select type (d => table%columns(i)%data)
+        type is (F32)
+            allocate(d%values(nrows))
+        type is (I32)
+            allocate(d%values(nrows))
+        class default
+            error stop "Unexpected type in allocate_table"
+      end select
+
     end do
 
   end subroutine allocate_table
@@ -51,6 +84,17 @@ contains
       deallocate(table%columns(i)%validity%bits)
       deallocate(table%columns(i)%offsets)
       deallocate(table%columns(i)%values)
+
+      select type (d => table%columns(i)%data)
+        type is (I32)
+          deallocate(d%values)
+        type is (F32)
+          deallocate(d%values)
+        class default
+            print *, "Warning: Unknown type during deallocation"
+      end select
+      deallocate(table%columns(i)%data)
+
     end do
 
     deallocate(table%columns)
@@ -67,17 +111,23 @@ contains
 
       length = size(keep_indices)
       if (size(t_table%columns) /= length) then
-        print *, "Column rows are not same length of current table rows."
+        print *, "Tables are not the same shape."
+        print *, "f_table: ", size(f_table%columns), " t_table: ", size(t_table%columns)
         err = 1
         return 
       end if
 
       do i = 1, length
-        print *, "C"
         j = keep_indices(i)
         t_table%columns(i)%validity%bits = f_table%columns(j)%validity%bits
         t_table%columns(i)%offsets = f_table%columns(j)%offsets
         t_table%columns(i)%values = f_table%columns(j)%values
+        select type (d => t_table%columns(i)%data)
+          type is (F32)
+            d%values = f_table%columns(j)%values
+          class default
+              error stop "Unexpected type in copy_table"
+        end select
       end do
       
     else 
@@ -86,6 +136,13 @@ contains
         t_table%columns(i)%validity%bits = f_table%columns(i)%validity%bits
         t_table%columns(i)%offsets = f_table%columns(i)%offsets
         t_table%columns(i)%values = f_table%columns(i)%values
+
+        select type (d => t_table%columns(i)%data)
+          type is (F32)
+            d%values = f_table%columns(i)%values
+          class default
+              error stop "Unexpected type in allocate_table"
+        end select
       end do
 
     end if 
@@ -106,11 +163,18 @@ contains
     if (.not. allocated(table%columns)) then 
       call allocate_table(table, nrows, ncols)
       table%columns(ncols)%values = values
+      
+      select type (d => table%columns(ncols)%data)
+          type is (F32)
+            d%values = values
+          class default
+              error stop "Unexpected type in allocate_table"
+      end select
       return
     end if 
     
     if (size(table%columns(1)%values) /= nrows) then
-      print *, "Column rows are not same length of current table rows."
+      print *, "New column is not same length of current table."
       err = 1
       return 
     end if
@@ -119,6 +183,12 @@ contains
     call copy_table(table, t_table)
     call destroy_table(table)
     t_table%columns(ncols)%values = values
+    select type (d => t_table%columns(ncols)%data)
+      type is (F32)
+        d%values = values
+      class default
+          error stop "Unexpected type in allocate_table"
+    end select
 
     call allocate_table(table, nrows, ncols)
     call copy_table(t_table, table)
